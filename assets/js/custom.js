08343da45c221a3d8bdd0eb84fd4cc09ce3f3ae1
Alexandre Heuillet <ale.heuillet@gmail.com>
2019-01-21 10:33:57 +0100

    Updated counter date

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,7 +44,7 @@
 	/*  2. EVENT TIME COUNTER
 	/* ----------------------------------------------------------- */
 	
-	$('#mu-event-counter').countdown('2018/03/24').on('update.countdown', function(event) {
+	$('#mu-event-counter').countdown('2019/03/23').on('update.countdown', function(event) {
 	  var $this = $(this).html(event.strftime(''
 	    + '<span class="mu-event-counter-block"><span>%D</span> Days</span> '
 	    + '<span class="mu-event-counter-block"><span>%H</span> Hours</span> '
